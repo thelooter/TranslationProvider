280b47cc7ee007128f3aeb9144000582573d2e2f
Lena Kolb <lenakolb2204@gmail.com>
2022-04-30 02:33:43 +0200

    Added Support for Kyori Components and bumped Version to 2.1.0-SNAPSHOT

--- Spigot/src/main/java/eu/tuxcraft/translationprovider/spigot/model/LazyLoadingMessage.java
+++ Spigot/src/main/java/eu/tuxcraft/translationprovider/spigot/model/LazyLoadingMessage.java
@@ -5,6 +5,8 @@ import eu.tuxcraft.translationprovider.spigot.TranslationProvider;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -18,66 +20,91 @@ import java.util.Map;
  * @since 2.0.0
  */
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class LazyLoadingMessage implements Message {
 
-  TranslationProviderEngine engine = TranslationProvider.getEngine();
+	TranslationProviderEngine engine = TranslationProvider.getEngine();
 
-  String key;
-  String prefixKey;
+	String key;
+	String prefixKey;
 
-  /**
-   * {@inheritDoc}
-   *
-   * @param cs {@inheritDoc}
-   * @since 2.0.0
-   */
-  @Override
-  public void sendTo(CommandSender cs) {
-    sendTo(cs, Map.of("", ""));
-  }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @param cs {@inheritDoc}
+	 * @since 2.0.0
+	 */
+	@Override
+	public void sendTo(CommandSender cs) {
+		sendTo(cs, Map.of("", ""));
+	}
 
-  @Override
-  public void sendTo(CommandSender cs, Map<String, String> params) {
+	@Override
+	public void sendTo(CommandSender cs, Map<String, String> params) {
 
-    String translation;
-    String prefix;
+		String translation;
+		String prefix;
 
-    if (cs instanceof Player player) {
-      translation = engine.getTranslationForUser(player.getUniqueId(), key, params);
-      prefix = engine.getTranslationForUser(player.getUniqueId(), prefixKey, params);
+		if (cs instanceof Player player) {
+			translation = engine.getTranslationForUser(player.getUniqueId(), key, params);
+			prefix = engine.getTranslationForUser(player.getUniqueId(), prefixKey, params);
 
-    }else{
-      translation = engine.getTranslationForUser(null, key, params);
-      prefix = engine.getTranslationForUser(null, prefixKey, params);
-    }
+		} else {
+			translation = engine.getTranslationForUser(null, key, params);
+			prefix = engine.getTranslationForUser(null, prefixKey, params);
+		}
 
-    if (prefix == null) {
-      prefix = "";
-    }
+		if (prefix == null) {
+			prefix = "";
+		}
 
-    if (translation.contains("\n")){
-      for (String line : translation.split("\n")) {
-        cs.sendMessage(prefix + line);
-      }
-    }else{
-      cs.sendMessage(prefix + translation);
-    }
+		if (translation.contains("\n")) {
+			for (String line : translation.split("\n")) {
+				cs.sendMessage(prefix + line);
+			}
+		} else {
+			cs.sendMessage(prefix + translation);
+		}
 
-  }
+	}
 
-  @Override
-  public String getFor(CommandSender cs) {
+	@Override
+	public String getFor(CommandSender cs) {
 
-    return getFor(cs, null);
-  }
+		return getFor(cs, null);
+	}
 
-  @Override
-  public String getFor(CommandSender cs, Map<String, String> params) {
-    if (cs instanceof Player player) {
-      return engine.getTranslationForUser(player.getUniqueId(), key, params);
-    }else{
-      return engine.getTranslationForUser(null, key, params);
-    }
-  }
+	@Override
+	public String getFor(CommandSender cs, Map<String, String> params) {
+		if (cs instanceof Player player) {
+			return engine.getTranslationForUser(player.getUniqueId(), key, params);
+		} else {
+			return engine.getTranslationForUser(null, key, params);
+		}
+	}
+
+	@Override public Component getComponentFor(CommandSender cs, boolean isMiniMessage) {
+		if (isMiniMessage) {
+			return MiniMessage.miniMessage().deserialize(getFor(cs, null));
+		} else {
+			return Component.text(getFor(cs, null));
+		}
+	}
+
+	@Override public Component getComponentFor(CommandSender cs, Map<String, String> params, boolean isMiniMessage) {
+		if (cs instanceof Player player) {
+			if (isMiniMessage) {
+				return MiniMessage.miniMessage().deserialize(engine.getTranslationForUser(player.getUniqueId(), key, params));
+			} else {
+				return Component.text(engine.getTranslationForUser(player.getUniqueId(), key, params));
+			}
+		} else {
+			if (isMiniMessage) {
+				return MiniMessage.miniMessage().deserialize(engine.getTranslationForUser(null, key, params));
+			} else {
+				return Component.text(engine.getTranslationForUser(null, key, params));
+			}
+		}
+
+	}
 }
--- Spigot/src/main/java/eu/tuxcraft/translationprovider/spigot/model/Message.java
+++ Spigot/src/main/java/eu/tuxcraft/translationprovider/spigot/model/Message.java
@@ -1,5 +1,7 @@
 package eu.tuxcraft.translationprovider.spigot.model;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
 
 import java.util.Map;
@@ -31,6 +33,7 @@ public interface Message {
 
   /**
    * Gets the translation for the given {@link CommandSender}
+   *
    * @param cs the {@link CommandSender} to get the translation for
    * @return the translation
    */
@@ -38,9 +41,29 @@ public interface Message {
 
   /**
    * Gets the translation for the given {@link CommandSender}
+   *
    * @param cs the {@link CommandSender} to get the translation for
    * @param params the replacements to use
    * @return the translation
    */
   String getFor(CommandSender cs, Map<String, String> params);
+
+  /**
+   * Gets the translation as a {@link Component} for the given {@link CommandSender}
+   *
+   * @param cs the {@link CommandSender} to get the translation for
+   * @param isMiniMessage whether the Translation should be parsed as a {@link MiniMessage}
+   * @return the translation as a {@link Component}
+   */
+  Component getComponentFor(CommandSender cs, boolean isMiniMessage);
+
+  /**
+   * Gets the translation as a {@link Component} for the given {@link CommandSender}
+   *
+   * @param cs the {@link CommandSender} to get the translation for
+   * @param params the replacements to use
+   * @param isMiniMessage whether the Translation should be parsed as a {@link MiniMessage}
+   * @return the translation as a {@link Component}
+   */
+  Component getComponentFor(CommandSender cs, Map<String, String> params, boolean isMiniMessage);
 }
