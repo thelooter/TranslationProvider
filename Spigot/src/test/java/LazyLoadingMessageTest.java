9845f625ed97e6011620303d2e916bf06ef6a491
Lena Kolb <lenakolb2204@gmail.com>
2022-05-19 21:35:30 +0200

    Add Tests

--- Spigot/src/test/java/LazyLoadingMessageTest.java
+++ Spigot/src/test/java/LazyLoadingMessageTest.java
@@ -9,25 +9,25 @@ import eu.tuxcraft.translationprovider.engine.model.Language;
 import eu.tuxcraft.translationprovider.spigot.TranslationProvider;
 import eu.tuxcraft.translationprovider.spigot.model.LazyLoadingMessage;
 import java.util.UUID;
-import org.junit.jupiter.api.BeforeAll;
+import org.bukkit.Bukkit;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LazyLoadingMessageTest {
 
-  static ServerMock server;
-  static TranslationProvider plugin;
+  ServerMock server;
+  TranslationProvider plugin;
 
-  static LazyLoadingMessage message;
+  LazyLoadingMessage message;
 
-
-  @BeforeAll
-  public static void setUp() {
+  @BeforeEach
+  public void setUp() {
 
     server = MockBukkit.mock();
     plugin = MockBukkit.load(TranslationProvider.class);
 
-    message = new LazyLoadingMessage("lobby.navigator.navigatorTitle", "lobby");
-
+    message = new LazyLoadingMessage("lobby.scoreBoard.rankTitle");
   }
 
   @Test
@@ -37,12 +37,49 @@ public class LazyLoadingMessageTest {
 
   @Test
   void testGetFor() {
-    PlayerMock player = new PlayerMock(server, "thelooter2204",
-        UUID.fromString("08fbc97b-93cd-4f2a-9369-29e025136b08"));
+    PlayerMock player =
+        new PlayerMock(
+            server, "thelooter2204", UUID.fromString("08fbc97b-93cd-4f2a-9369-29e025136b08"));
 
     TranslationProvider.playerLanguage(player, Language.fromDisplayName("English"));
 
     assertThat(message.getFor(player), instanceOf(String.class));
-    assertThat(message.getFor(player), equalToIgnoringCase("Navigator"));
+    assertThat(message.getFor(player), equalToIgnoringCase("Rank:"));
+  }
+
+  @Test
+  void testGetForConsoleSender() {
+
+    assertThat(message.getFor(Bukkit.getConsoleSender()),equalToIgnoringCase("Rang:"));
+  }
+
+  @Test
+  void testSendTo() {
+    PlayerMock player =
+        new PlayerMock(
+            server, "thelooter2204", UUID.fromString("08fbc97b-93cd-4f2a-9369-29e025136b08"));
+
+    server.addPlayer(player);
+
+    TranslationProvider.playerLanguage(player, Language.fromDisplayName("English"));
+
+    message.sendTo(player);
+
+    player.assertSaid("Rank:");
+  }
+
+  @AfterEach
+  public void tearDown() {
+
+    PlayerMock player =
+        new PlayerMock(
+            server, "thelooter2204", UUID.fromString("08fbc97b-93cd-4f2a-9369-29e025136b08"));
+
+    server.addPlayer(player);
+
+    TranslationProvider.playerLanguage(player, Language.fromDisplayName("Deutsch"));
+
+    MockBukkit.unmock();
+
   }
 }
